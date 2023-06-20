35be6d40f7f39cae88a77e0774b0a8f7f6b13d0f
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-06-20 10:16:32 -0400

    Added Prisma boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
+dotenv.config();
 const app: Application = express();
 
+app.use(
+	cors({
+		origin:
+			process.env.NODE_ENV === 'production'
+				? process.env.PROD_URL
+				: '<http://localhost:3000>',
+	})
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
